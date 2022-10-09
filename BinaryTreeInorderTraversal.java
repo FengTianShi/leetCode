2920735bdc8333a251d9bd597adecfac913b1324
FengTianShi <fts2470216@gmail.com>
2022-10-09 20:03:29 +0900

    Update BinaryTreeInorderTraversal.java

--- BinaryTreeInorderTraversal.java
+++ BinaryTreeInorderTraversal.java
@@ -1,14 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.tree.TreeNode;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 public class BinaryTreeInorderTraversal {
     public static void main(String[] args) {
-
+        BinaryTreeInorderTraversal binaryTreeInorderTraversal = new BinaryTreeInorderTraversal();
+        TreeNode node3 = new TreeNode(5, null, null);
+        TreeNode node4 = new TreeNode(6, null, null);
+        TreeNode node5 = new TreeNode(7, null, null);
+        TreeNode node1 = new TreeNode(3, node3, node4);
+        TreeNode node2 = new TreeNode(4, null, node5);
+        TreeNode root = new TreeNode(2, node1, node2);
+        System.out.println(binaryTreeInorderTraversal.inorderTraversal(root).toString());
     }
 
+    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        return new ArrayList<Integer>();
+        doInorderTraversal(root);
+        return arrayList;
+    }
+
+    public void doInorderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        doInorderTraversal(node.left);
+        arrayList.add(node.val);
+        doInorderTraversal(node.right);
     }
 }
